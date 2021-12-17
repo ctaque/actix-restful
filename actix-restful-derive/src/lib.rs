c8601b236ebdda7c8270e4541e731252396fae5e
Cyprien Taque <cyprien.taque@gmail.com>
2021-12-17 20:10:19 +0100

    add lib code

--- actix-restful-derive/src/lib.rs
+++ actix-restful-derive/src/lib.rs
@@ -1,8 +1,228 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+extern crate proc_macro;
+use quote::quote;
+use syn::{ self, Result as SynResult, Token};
+
+struct HttpCreateDeriveParams (syn::Ident);
+impl syn::parse::Parse for HttpCreateDeriveParams {
+    fn parse(input: syn::parse::ParseStream) -> SynResult<Self> {
+        let content;
+        syn::parenthesized!(content in input);
+        let query = content.parse()?;
+        Ok(HttpCreateDeriveParams(query))
     }
+}
+#[proc_macro_derive(HttpCreate, attributes(http_create))]
+pub fn http_create(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let ast = syn::parse(input).unwrap();
+    impl_http_create_macro(&ast)
+}
+
+fn impl_http_create_macro(ast: &syn::DeriveInput) -> proc_macro::TokenStream {
+    let attribute = ast.attrs.iter().filter(
+        |a| a.path.segments.len() == 1 && a.path.segments[0].ident == "http_create"
+    ).nth(0).expect("http_create attribute required for deriving HttpCreate!");
+
+    let parameter: HttpCreateDeriveParams = syn::parse2(attribute.tokens.clone()).expect("Invalid http_create attribute!");
+    let HttpCreateDeriveParams(query) = parameter;
+
+    let name = &ast.ident;
+    let gen = quote! {
+        #[async_trait]
+        impl HttpCreate<#name, #query> for #name {
+            async fn http_create(payload: web::Json<#name>, query: web::Query<#query>) -> Result<HttpResponse, HttpResponse>{
+                let params = query.into_inner();
+                let to_save = payload.into_inner();
+                let result = to_save.save(&params).await;
+                match result {
+                    Ok(res) => Ok(HttpResponse::Ok().body(json!(res))),
+                    Err(err) => Err(HttpResponse::InternalServerError().body(err.to_string()))
+                }
+            }
+        }
+    };
+    gen.into()
+}
+
+struct HttpAllDeriveParams (syn::Ident);
+impl syn::parse::Parse for HttpAllDeriveParams {
+    fn parse(input: syn::parse::ParseStream) -> SynResult<Self> {
+        let content;
+        syn::parenthesized!(content in input);
+        let query = content.parse()?;
+        Ok(HttpAllDeriveParams(query))
+    }
+}
+#[proc_macro_derive(HttpAll, attributes(http_all))]
+pub fn http_all(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let ast = syn::parse(input).unwrap();
+    impl_http_all_macro(&ast)
+}
+
+fn impl_http_all_macro(ast: &syn::DeriveInput) -> proc_macro::TokenStream {
+    let attribute = ast.attrs.iter().filter(
+        |a| a.path.segments.len() == 1 && a.path.segments[0].ident == "http_all"
+    ).nth(0).expect("http_all attribute required for deriving HttpAll!");
+
+    let parameter: HttpAllDeriveParams = syn::parse2(attribute.tokens.clone()).expect("Invalid http_all attribute!");
+    let HttpAllDeriveParams(query) = parameter;
+
+    let name = &ast.ident;
+    let gen = quote! {
+        #[async_trait]
+        impl HttpAll<#query> for #name {
+            async fn http_all(query: web::Query<#query>) -> Result<HttpResponse, HttpResponse>{
+                let params = query.into_inner();
+                let result = #name::all(&params).await;
+                match result {
+                    Ok(res) => Ok(HttpResponse::Ok().body(json!(res))),
+                    Err(err) => Err(HttpResponse::InternalServerError().body(err.to_string()))
+                }
+            }
+        }
+    };
+    gen.into()
+}
+
+struct HttpFindDeriveParams (syn::Ident, syn::Ident);
+impl syn::parse::Parse for HttpFindDeriveParams {
+    fn parse(input: syn::parse::ParseStream) -> SynResult<Self> {
+        let content;
+        syn::parenthesized!(content in input);
+        let path = content.parse()?;
+        content.parse::<Token![,]>()?;
+        let query = content.parse()?;
+        Ok(HttpFindDeriveParams(path, query))
+    }
+}
+
+#[proc_macro_derive(HttpFind, attributes(http_find))]
+pub fn http_find(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let ast = syn::parse(input).unwrap();
+    impl_http_find_macro(&ast)
+}
+
+fn impl_http_find_macro(ast: &syn::DeriveInput) -> proc_macro::TokenStream {
+    let attribute = ast.attrs.iter().filter(
+        |a| a.path.segments.len() == 1 && a.path.segments[0].ident == "http_find"
+    ).nth(0).expect("http_find attribute required for deriving HttpFind!");
+
+    let parameter: HttpFindDeriveParams = syn::parse2(attribute.tokens.clone()).expect("Invalid http_find attribute!");
+    let HttpFindDeriveParams(path, query) = parameter;
+
+    let name = &ast.ident;
+    let gen = quote! {
+        #[async_trait]
+        impl HttpFind<#path, #query> for #name {
+            async fn http_find(info: web::Path<#path>, query: web::Query<#query>) -> Result<HttpResponse, HttpResponse> {
+                let params = query.into_inner();
+                let result = #name::find(info.id.into(), &params).await;
+                match result {
+                    Ok(res) => Ok(HttpResponse::Ok().body(json!(res))),
+                    Err(err) => Err(HttpResponse::NotFound().body("ENTITY_NOT_FOUND"))
+                }
+            }
+        }
+    };
+    gen.into()
+}
+
+struct HttpDeleteDeriveParams (syn::Ident, syn::Ident);
+impl syn::parse::Parse for HttpDeleteDeriveParams {
+    fn parse(input: syn::parse::ParseStream) -> SynResult<Self> {
+        let content;
+        syn::parenthesized!(content in input);
+        let path = content.parse()?;
+        content.parse::<Token![,]>()?;
+        let query = content.parse()?;
+        Ok(HttpDeleteDeriveParams(path, query))
+    }
+}
+
+#[proc_macro_derive(HttpDelete, attributes(http_delete))]
+pub fn http_delete(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let ast = syn::parse(input).unwrap();
+    impl_http_delete_macro(&ast)
+}
+
+fn impl_http_delete_macro(ast: &syn::DeriveInput) -> proc_macro::TokenStream {
+    let attribute = ast.attrs.iter().filter(
+        |a| a.path.segments.len() == 1 && a.path.segments[0].ident == "http_delete"
+    ).nth(0).expect("http_delete attribute required for deriving HttpDelete!");
+
+    let parameter: HttpDeleteDeriveParams = syn::parse2(attribute.tokens.clone()).expect("Invalid http_delete attribute!");
+    let HttpDeleteDeriveParams(path, query) = parameter;
+
+    let name = &ast.ident;
+    let gen = quote! {
+        #[async_trait]
+        impl HttpDelete<#path, #query> for #name {
+            async fn http_delete(info: web::Path<#path>, query: web::Query<#query>) -> Result<HttpResponse, HttpResponse> {
+                let params = query.into_inner();
+                let result = #name::find(info.id.into(), &params).await;
+
+                match result {
+                    Ok(entity) => {
+                        match entity.delete(&params).await {
+                            Ok(e) => Ok(HttpResponse::Ok().body(json!(e))),
+                            Err(err) => Err(HttpResponse::InternalServerError().body(err.to_string()))
+                        }
+                    }
+                    Err(err) => Err(HttpResponse::NotFound().body("ENTITY_NOT_FOUND"))
+                }
+            }
+        }
+    };
+    gen.into()
+}
+
+struct HttpUpdateDeriveParams (syn::Ident, syn::Ident, syn::Ident);
+impl syn::parse::Parse for HttpUpdateDeriveParams {
+    fn parse(input: syn::parse::ParseStream) -> SynResult<Self> {
+        let content;
+        syn::parenthesized!(content in input);
+        let path = content.parse()?;
+        content.parse::<syn::Token![,]>()?;
+        let query = content.parse()?;
+        content.parse::<syn::Token![,]>()?;
+        let output = content.parse()?;
+        Ok(HttpUpdateDeriveParams(path, query, output))
+    }
+}
+
+#[proc_macro_derive(HttpUpdate, attributes(http_update))]
+pub fn http_update(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let ast = syn::parse(input).unwrap();
+    impl_http_update_macro(&ast)
+}
+
+fn impl_http_update_macro(ast: &syn::DeriveInput) -> proc_macro::TokenStream {
+    let attribute = ast.attrs.iter().filter(
+        |a| a.path.segments.len() == 1 && a.path.segments[0].ident == "http_update"
+    ).nth(0).expect("http_update attribute required for deriving HttpUpdate!");
+
+    let parameter: HttpUpdateDeriveParams = syn::parse2(attribute.tokens.clone()).expect("Invalid http_update attribute!");
+    let HttpUpdateDeriveParams(path, query, output) = parameter;
+
+    let name = &ast.ident;
+    let gen = quote! {
+        #[async_trait]
+        impl HttpUpdate<#path, #query, #output> for #name {
+            async fn http_update(info: web::Path<#path>, payload: web::Json<Box<#name>>, query: web::Query<#query>) -> Result<HttpResponse, HttpResponse> {
+                let entity = payload.into_inner();
+                let params = query.into_inner();
+                let result: = #output::find(info.id.into(), &params).await;
+
+                match result {
+                    Ok(entity) => {
+                        match entity.update(&params).await {
+                            Ok(e) => Ok(HttpResponse::Ok().body(json!(e))),
+                            Err(err) => Err(HttpResponse::InternalServerError().body(err.to_string()))
+                        }
+                    }
+                    Err(err) => Err(HttpResponse::NotFound().body("ENTITY_NOT_FOUND"))
+                }
+            }
+        }
+    };
+    gen.into()
 }
--- actix-restful/src/lib.rs
+++ actix-restful/src/lib.rs
@@ -1,8 +1,46 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+use async_trait::async_trait;
+use actix_web::{ HttpResponse, web };
+use anyhow::Result;
+
+#[async_trait]
+pub trait Model<T, FP, FQ, AQ, AR, DQ> {
+    async fn find(id: FP, query: &FQ) -> Result<T>;
+    async fn all(query: &AQ) -> Result<AR>;
+    async fn delete(self: Self, query: &DQ) -> Result<T>;
+}
+
+#[async_trait]
+pub trait NewModel<T, Q> {
+    async fn save(self: Self, query: &Q) -> Result<T>;
+}
+
+#[async_trait]
+pub trait UpdatableModel<T, Q> {
+    async fn update(self: Self, query: &Q) -> Result<T>;
+}
+
+#[async_trait]
+pub trait HttpCreate<T, Q> {
+    async fn http_create(payload: web::Json<T>, query: web::Query<Q>) -> Result<HttpResponse, HttpResponse>;
+}
+
+#[async_trait]
+pub trait HttpAll<Q> {
+    async fn http_all(query: web::Query<Q>) -> Result<HttpResponse, HttpResponse>;
+}
+
+#[async_trait]
+pub trait HttpFind<P, Q> {
+    async fn http_find(info: web::Path<P>, query: web::Query<Q>) -> Result<HttpResponse, HttpResponse>;
+}
+
+
+#[async_trait]
+pub trait HttpDelete<P, Q> {
+    async fn http_delete(info: web::Path<P>, query: web::Query<Q>) -> Result<HttpResponse, HttpResponse>;
+}
+
+#[async_trait]
+pub trait HttpUpdate<P, Q, T> {
+    async fn http_update(info: web::Path<P>, query: web::Query<Q>, payload: web::Json<Box<Self>>) -> Result<HttpResponse, HttpResponse>;
 }
